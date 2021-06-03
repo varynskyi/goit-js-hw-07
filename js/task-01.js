0b152551c2e1e8347ac21e8d805ef7a4e70adc63
Александр <64688166+varynskyi@users.noreply.github.com>
2021-06-03 22:24:10 +0300

    completed
    task#1

--- js/task-01.js
+++ js/task-01.js
@@ -2,11 +2,11 @@ const amountCategories = document.querySelectorAll('li.item');
 console.log(`В списке ${amountCategories.length} категории`);
 
 for (let i = 0; i < amountCategories.length; i += 1) {
-    const theThirdTitle = amountCategories[i].querySelector('h2');
-console.log(`Категория: ${theThirdTitle.textContent}`);
+    const title = amountCategories[i].querySelector('h2');
+console.log(`Категория: ${title.textContent}`);
 
-const theThirdItem = amountCategories[i].querySelectorAll('li');
-    console.log(`Колличество элементов: ${theThirdItem.length}`);
+const item = amountCategories[i].querySelectorAll('li');
+    console.log(`Колличество элементов: ${item.length}`);
     
 }
 
