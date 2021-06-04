cceda193065b7a66becd26094418df140ffcfd23
Александр <64688166+varynskyi@users.noreply.github.com>
2021-06-04 08:24:03 +0300

    completed
    task#1

--- js/task-01.js
+++ js/task-01.js
@@ -1,14 +1,22 @@
+//1
 const amountCategories = document.querySelectorAll('li.item');
 console.log(`В списке ${amountCategories.length} категории`);
 
 for (let i = 0; i < amountCategories.length; i += 1) {
     const title = amountCategories[i].querySelector('h2');
-console.log(`Категория: ${title.textContent}`);
-
-const item = amountCategories[i].querySelectorAll('li');
-    console.log(`Колличество элементов: ${item.length}`);
-    
+    const item = amountCategories[i].querySelectorAll('li');
+    console.log(`Категория: ${title.textContent}
+Колличество элементов: ${item.length}`);
+ 
 }
 
+//2
+const totalCategories = document.querySelectorAll(".item");
+console.log(`В списке ${totalCategories.length} категории.`);
 
-
+const categoriesArray = [...totalCategories]
+    .map(
+        categories => {
+            console.log(`Категория: ${categories.children[0].textContent}
+Количество элементов: ${categories.children[1].children.length}`
+    )});
